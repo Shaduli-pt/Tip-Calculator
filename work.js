e89650d407af4a924ff47ddb696da22611ab998b
shaduli-pt <shaduliptmjy2001@gmail.com>
2025-04-22 16:14:21 +0530

    style modified

--- work.js
+++ work.js
@@ -5,13 +5,13 @@ function calculate(){
     }
     else{
         let billPrice = parseFloat(document.getElementById("billAmount").value)
-    let tipPerc = parseFloat(document.getElementById("tip").value)
+        let tipPerc = parseFloat(document.getElementById("tip").value)
 
-    let tipAmount = (billPrice*tipPerc)/100;
-    let totalAmnt = billPrice+tipAmount;
+        let tipAmount = (billPrice*tipPerc)/100;
+        let totalAmnt = billPrice+tipAmount;
 
-    document.getElementById('tipamount').textContent=tipAmount.toFixed(2);
-    document.getElementById('totalbill').textContent=totalAmnt.toFixed(2);
+        document.getElementById('tipamount').textContent=tipAmount.toFixed(2)+ " ₹";
+        document.getElementById('totalbill').textContent=totalAmnt.toFixed(2)+ " ₹";
     }
     
 }
